28875cb20a0ffc723eab0d78b094d5e46f1c81ac
oscarinformatico <oscartroyanoinformatica@gmail.com>
2015-01-07 14:13:29 -0430

    first commit

---
+++ JW.ORG-Menu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# JW.ORG install menu listas por el internet :D Hacksordo
+# passwd clave user
+sudo su
+# movie images tres fotos a icons
+echo "movie images a icons"
+cp -a JW /usr/share/icons/
+# Install Menu Jw movie a APPLICATIONS 
+echo "Install Menu Jw movie a APPLICATIONS"
+cd Aplicaciones
+cp *.desktop /usr/share/applications/
